f02709c85616bd4797dd3f4c3802f06c73fb46b5
S00210326 <s00210326@atu.ie>
2023-06-12 14:38:53 +0200

    Initial commit

---
+++ temp.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#Recurrent Neural netowrk
+#Part 1 - Data Preprocessing
+
+
+#importing the libraries
+import numpy as np#
+import matplotlib.pyplot as plt
+import pandas
+#importing the training set
+
+#Part 2 - Building the RNN
+
+
+#Part 3 - M
